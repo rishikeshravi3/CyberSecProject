a6c4f854585756c835ebe314afa4f71950b06acb
rishikeshravi3 <137996382+rishikeshravi3@users.noreply.github.com>
2024-07-10 19:46:08 -0400

    initial commit

---
+++ xss-demo/src/main/java/com/example/xssdemo/XssController.java
@@ -0,0 +1,16 @@
+package com.example.xssdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class XssController {
+
+    @GetMapping("/greet")
+    public String greet(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greet";
+    }
+}
---
+++ xss-demo/src/main/java/com/example/xssdemo/XssDemoApplication.java
@@ -0,0 +1,11 @@
+package com.example.xssdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XssDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XssDemoApplication.class, args);
+    }
+}
